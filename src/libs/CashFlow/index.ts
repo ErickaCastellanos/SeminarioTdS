c39bd407697310d380c358286bb6fa3c930797dc
Ericka Castellanos <65922114+ErickaCastellanos@users.noreply.github.com>
2022-09-27 11:16:54 -0600

    Agregando las funciones del CRUD en el archivo CashFlow

--- src/libs/CashFlow/index.ts
+++ src/libs/CashFlow/index.ts
@@ -12,6 +12,18 @@ export class CashFlow {
     private cashFlowItems : ICashFlow[] = [];
 
     //Elementos
+
+    public getAllCashFlow(): ICashFlow[] {
+        return this.cashFlowItems; // select * from cashflow;
+    }
+    
+    public getCashFlowByIndex( index:number): ICashFlow {
+        if (index >= 0 && index < this.cashFlowItems.length) {
+          return this.cashFlowItems[index];
+        }
+        throw Error('Index out of range');
+    }
+
     public addCashFlow(cashFlow:ICashFlow) : number {
         const cashFlowExist = this.cashFlowItems.findIndex(
             (obj) =>{
@@ -22,7 +34,28 @@ export class CashFlow {
         if(cashFlowExist < 0){
             this.cashFlowItems.push(cashFlow);
             return this.cashFlowItems.length -1;
+            // [{},{},{},{}]
+            // 0   1   2   3
+            // 4 - 1 = 3
         }
         throw Error('CashFlow Exists on Collection');
+    }
+
+    public updateCashFlow( index:number, cashFlow:ICashFlow): boolean {
+        if (index >= 0 && index < this.cashFlowItems.length) {
+          this.cashFlowItems[index] = cashFlow;
+          return true;
+        }
+        return false;
+    }
+    
+    public deleteCashFlow( index:number): boolean {
+    if ( index >= 0 && index < this.cashFlowItems.length ) {
+        this.cashFlowItems = this.cashFlowItems.filter(
+        (_obj: ICashFlow, i:number)=> i !== index
+        );
+        return true;
+    }
+    return false;
     }
 }
