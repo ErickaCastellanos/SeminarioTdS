f5d47d3724176ed127934f6bd4b6776934346f2b
Ericka Castellanos <65922114+ErickaCastellanos@users.noreply.github.com>
2022-10-16 00:46:11 -0600

    Inicio de conexión a MongoDB y las variables .env

---
+++ src/dao/models/mongodb/MongoDBConn.ts
@@ -0,0 +1,19 @@
+//Para establecer la conexión
+import { MongoClient } from "mongodb";
+
+//Realizar una conexion para poder conectarme
+let connection:MongoClient = null;
+//Por si no se conecta que se conecte a una local
+let mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017'; 
+let mongoDBName = process.env.MONGO_DB_NAME || 'sw202203';
+
+//Vamos a exportar elementos que nos devuelven una conexión
+export const gettConnection = async ()=> {
+    //
+    if (!connection) {
+        connection = await MongoClient.connect(mongoURI);
+    }
+    return{
+        connection, db: connection.db(mongoDBName)
+    }
+}
