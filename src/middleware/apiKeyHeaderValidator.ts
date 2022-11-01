9e285f57a2c2d67294c0e693a4cb2ebb2abd676c
Ericka Castellanos <65922114+ErickaCastellanos@users.noreply.github.com>
2022-11-01 10:02:32 -0600

    Me faltaba el return en el next() de apiKeyHeaderValidator

--- src/middleware/apiKeyHeaderValidator.ts
+++ src/middleware/apiKeyHeaderValidator.ts
@@ -2,9 +2,9 @@
 import validateApiKey from "@server/utils/apiKeyValidator";
 
 const apiKeyMW = (req, res, next)=>{
-   const apikey =  req.get('apikey') || ''; 
+   const apikey = req.get('apikey') || ''; 
    if (validateApiKey(apikey)) {
-        next();// pase al siguiente paso
+      return next();// pase al siguiente paso
    }
 
    return res.status(406).json({"error":"APIKEY Not valid!"});
